3bffec8e9db023686a10c2ae29deea7045fc7445
takeshi_honda <honda0924@hotmail.co.jp>
2022-01-16 21:40:02 +0900

    feat: crud

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -21,4 +21,46 @@ class TaskTest extends TestCase
         $response->assertOk()
             ->assertJsonCount($tasks->count());
     }
+
+    /**
+     * @test
+     */
+    public function successRegister()
+    {
+        $data = [
+            'title' => 'テスト投稿'
+        ];
+        $response = $this->postJson('api/tasks', $data);
+
+        $response->assertStatus(201)
+            ->assertJsonFragment($data);
+    }
+    /**
+     * @test
+     */
+    public function successUpdate()
+    {
+        $task = Task::factory()->create();
+        $task->title = '更新タイトル';
+        $response = $this->patchJson("api/tasks/{$task->id}", $task->toArray());
+        $response->assertOk()
+            ->assertJsonFragment($task->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function successDelete()
+    {
+        $tasks = Task::factory()->count(10)->create();
+
+        $response = $this->deleteJson("api/tasks/{$tasks[0]->id}");
+        $response->assertOk();
+
+        $response = $this->getJson('api/tasks');
+        $response->assertJsonCount($tasks->count() - 1);
+
+
+        // ->assertJsonFragment($task->toArray());
+    }
 }
