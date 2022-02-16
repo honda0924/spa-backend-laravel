4ed9b520b2d33f3f32df87e86f66f0cd9b1b5dff
takeshi_honda <honda0924@hotmail.co.jp>
2022-02-17 01:23:59 +0900

    feat: update is_done

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::apiResource('tasks', 'TaskController');
+Route::patch('tasks/update-done/{task}', 'TaskController@updateDone')->name('tasks.isDone');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
